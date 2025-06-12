7a993486d9280b6f9963b3d168ec63da9ebb433b
S0okJu <damekyung98@gmail.com>
2025-06-12 16:45:44 +0900

    [Test] class visitor test code

--- pyclassanalyzer/visitors/__init__.py
+++ pyclassanalyzer/visitors/__init__.py
@@ -1,5 +1,5 @@
-# from .imports import ImportVisitor
-# from .classes import ClassVisitor
-# from .base import BaseVisitor
+from .imports import ImportVisitor
+from .classes import ClassVisitor
+from .base import BaseVisitor
 
-# __all__ = ['ImportVisitor', 'ClassVisitor', 'BaseVisitor']
+__all__ = ['ImportVisitor', 'ClassVisitor', 'BaseVisitor']
